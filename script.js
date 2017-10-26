b29b1377df223f06a9593dd5d527c745f7f39217
Rodrigo <33107028+rodwritescode@users.noreply.github.com>
2017-10-25 20:59:48 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var userFirst = prompt("Hi, what is your first name?");
+var userLast = prompt("Great, what is your last name?");
+var userAge = prompt("What a nice name! How about your age?");
+
+var fullName = userFirst + " " + userLast;
+
+console.log("It's nice to meet you " + fullName);
+console.log("Wow you're only " + userAge + " years old??");
